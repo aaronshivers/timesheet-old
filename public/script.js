45adb9bcc93ad4e3a50102767d01e23a747558da
Aaron Shivers <aaron@moonmail.org>
2019-02-02 17:40:42 -0600

    Setup server and index.html

---
+++ public/script.js
@@ -0,0 +1,9 @@
+// Job Class: Represents a Job
+class Job {
+  constructor(customer, description, timeIn, timeOut) {
+    this.customer = customer
+    this.description = description
+    this.timeIn = timeIn
+    this.timeOut = timeOut
+  }
+}
